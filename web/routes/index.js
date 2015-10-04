b4b0df6dcb08f8c8df2b9428ce837269c68860be
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-10-04 09:48:21 -0400

    Yay

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Essay Better Maker' });
 });
 
 router.post('/sendSurvey', function(req, res, next){
