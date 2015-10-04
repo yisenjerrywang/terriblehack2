4caaad1e2a009f627c315142be6ef1de2a2cf12e
Jerry Wang <yisenjerrywang@gmail.com>
2015-10-04 13:40:11 -0400

    repluralization

--- web/saurus.js
+++ web/saurus.js
@@ -15,21 +15,38 @@ function maxSaurus(text, callback) {
         for(i in words) {
             var processedWord = "";
 
+            var plural = false;
             var word = words[i];
             var lowerWord = word.toLowerCase();
             var filteredWord = lowerWord.replace(/[^\w\s]|_/g,"");
+            var finalWord = filteredWord;
 
-            var synonyms = thesaurus.search(filteredWord);
-            if (synonyms.length == 0) {
-                console.error("No synonyms found for " + filteredWord);
+            var singularWord = pluralize(filteredWord, 1);
+            if(singularWord != filteredWord) {
+                plural = true;
+                finalWord = singularWord;
+            }
+
+            var synonyms = thesaurus.search(finalWord);
+            if(synonyms.length == 0) {
+                console.log("No synonyms found for " + finalWord);
                 processedWord = word;
             }
             else {
                 synonyms.sort(function(a, b) {
                     return b.length - a.length;
                 });
-                console.log(lowerWord.indexOf(filteredWord));
-                processedWord = lowerWord.replace(filteredWord, synonyms[0]);
+                var count = 0;
+                while(synonyms[count].indexOf(" ") > 0) {
+                    count++;
+                }
+                var bestSynonym = synonyms[count];
+                if(plural == true && bestSynonym.indexOf("-") < 0 && bestSynonym.slice(-1) != "s") {
+                    console.log("pluralizing: " + bestSynonym);
+                    bestSynonym = pluralize(bestSynonym);
+                }
+                console.log("Lower word: " + lowerWord + " Final word: " + finalWord + " bestSynonym: " + bestSynonym);
+                processedWord = lowerWord.replace(filteredWord, bestSynonym);
             }
 
             newText = newText + " " + processedWord;
